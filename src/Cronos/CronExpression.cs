14b434b28abfe7ff0395c52df1496e251cf6b311
Andrey Dorokhov <aidmsu@gmail.com>
2017-01-12 12:52:07 +0300

    First test passed.

--- src/Cronos/CronExpression.cs
+++ src/Cronos/CronExpression.cs
@@ -1,7 +1,17 @@
-﻿namespace Cronos
+﻿using System;
+
+namespace Cronos
 {
     public class CronExpression
     {
-        
+        public static CronExpression Parse(string s)
+        {
+            return new CronExpression();
+        }
+
+        public bool IsMatch(DateTime dateTime)
+        {
+            return true;
+        }
     }
 }
