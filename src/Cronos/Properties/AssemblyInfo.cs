db44db53a08d18bf5df3c4c987efac7f1d2d891e
Andrey Dorokhov <aidmsu@gmail.com>
2017-01-12 12:30:48 +0300

    Add empty solution, core project, tests project.

---
+++ src/Cronos/Properties/AssemblyInfo.cs
@@ -0,0 +1,10 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Cronos")]
+[assembly: Guid("734a5ea7-d610-4ea1-b37e-a9d59c82b060")]
+[assembly: InternalsVisibleTo("Cronos.Tests")]
