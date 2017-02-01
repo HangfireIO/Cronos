f4e70cf38cddfafecfd906298cd0ce9f453a1d75
Andrey Dorokhov <aidmsu@gmail.com>
2017-02-01 14:40:31 +0300

    Remove unimplemented WhenReboot flag.

--- src/Cronos/CronExpressionFlag.cs
+++ src/Cronos/CronExpressionFlag.cs
@@ -8,11 +8,10 @@ namespace Cronos
         None = 0x0,
         DayOfMonthStar = 0x1,
         DayOfWeekStar = 0x2,
-        WhenReboot = 0x4, // TODO: Remove this
-        MinuteStar = 0x8,
-        HourStar = 0x10,
-        SecondStar = 0x20,
-        DayOfMonthLast = 0x40,
-        DayOfWeekLast = 0x80,
+        MinuteStar = 0x4,
+        HourStar = 0x8,
+        SecondStar = 0x10,
+        DayOfMonthLast = 0x20,
+        DayOfWeekLast = 0x40,
     }
 }
