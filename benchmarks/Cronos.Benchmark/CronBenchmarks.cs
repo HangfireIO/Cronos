9194e4dec072929446ba114fcdacb94a3d1b47d6
Andrey Dorokhov <aidmsu@gmail.com>
2017-03-10 13:16:25 +0300

    Move Cronos.Benchmark to a new project system

--- benchmarks/Cronos.Benchmark/CronBenchmarks.cs
+++ benchmarks/Cronos.Benchmark/CronBenchmarks.cs
@@ -1,16 +1,27 @@
 ﻿using System;
 using BenchmarkDotNet.Attributes;
 using BenchmarkDotNet.Attributes.Jobs;
+using NCrontab;
 
 namespace Cronos.Benchmark
 {
     [RyuJitX64Job]
     public class CronBenchmarks
     {
-        private static readonly CronExpression SimpleExpression = CronExpression.Parse("* * * * ?");
+        private static readonly CronExpression MinutlyExpression = CronExpression.Parse("* * * * *");
+        private static readonly CronExpression HourlyExpression = CronExpression.Parse("0 * * * *");
+        private static readonly CronExpression DaylyExpression = CronExpression.Parse("0 3 * * *");
+        private static readonly CronExpression MonthlyExpression = CronExpression.Parse("0 6 1 * *");
+
+        private static readonly CrontabSchedule NCrontabSimpleExpression = NCrontab.CrontabSchedule.Parse("* * * * *", new CrontabSchedule.ParseOptions{IncludingSeconds = false});
         private static readonly CronExpression ComplexExpression = CronExpression.Parse("*/10 12-20 * DEC 3");
 
-        private static readonly DateTime DateTimeNow = DateTime.Now;
+        private static readonly DateTimeOffset DateTimeNow = DateTimeOffset.UtcNow.Date;
+        private static readonly DateTime DateTimeNow1 = DateTime.UtcNow.Date;
+        private static readonly DateTimeOffset DateTimeNow2 = DateTimeOffset.UtcNow.AddMinutes(3);
+        private static readonly DateTimeOffset DateTimeNow3 = DateTimeOffset.UtcNow.AddMinutes(7);
+        private static readonly DateTimeOffset EndDateTime  = DateTimeNow.AddYears(100);
+        private static readonly DateTime EndDateTime1 = DateTimeNow1.AddYears(100);
 
         [Benchmark]
         public unsafe string ParseBaseline()
@@ -55,15 +66,51 @@ namespace Cronos.Benchmark
         }
 
         [Benchmark]
-        public DateTimeOffset? NextSimple()
+        public DateTimeOffset? GetOccurrenceSimpleDateTime()
         {
-            return SimpleExpression.GetOccurrence(DateTimeNow, DateTimeNow.AddYears(100), TimeZoneInfo.Utc);
+            return MinutlyExpression.GetOccurrence(DateTimeNow1, EndDateTime1, TimeZoneInfo.Utc);
         }
 
         [Benchmark]
-        public DateTimeOffset? NextComplex()
+        public DateTimeOffset? GetOccurrenceComplexDateTime()
         {
-            return ComplexExpression.GetOccurrence(DateTimeNow, DateTimeNow.AddYears(100), TimeZoneInfo.Utc);
+            return ComplexExpression.GetOccurrence(DateTimeNow, EndDateTime, TimeZoneInfo.Utc);
         }
+
+        [Benchmark]
+        public DateTimeOffset? GetOccurrenceSimpleDateTimeOffset()
+        {
+            return MinutlyExpression.GetOccurrence(DateTimeNow1, EndDateTime1, TimeZoneInfo.Utc);
+        }
+
+        [Benchmark]
+        public DateTimeOffset? GetOccurrenceComplexDateTimeOffset()
+        {
+            return ComplexExpression.GetOccurrence(DateTimeNow, EndDateTime, TimeZoneInfo.Utc);
+        }
+
+        //[Benchmark]
+        public DateTimeOffset? NCrontabSimple()
+        {
+            return NCrontabSimpleExpression.GetNextOccurrence(DateTimeNow1);
+        }
+        
+        [Benchmark]
+        public DateTimeOffset? GetOccurenceMinutelyOffset()
+        {
+            return HourlyExpression.GetOccurrence(DateTimeNow, EndDateTime, TimeZoneInfo.Utc);
+        }
+
+        //[Benchmark]
+        //public DateTimeOffset? GetOccurenceDayly()
+        //{
+        //    return DaylyExpression.GetOccurrence(DateTimeNow3, EndDateTime, TimeZoneInfo.Utc);
+        //}
+
+        //[Benchmark]
+        //public DateTimeOffset? GetOccurenceMonthly()
+        //{
+        //    return MonthlyExpression.GetOccurrence(DateTimeNow3, EndDateTime, TimeZoneInfo.Utc);
+        //}
     }
 }
--- benchmarks/Cronos.Benchmark/Program.cs
+++ benchmarks/Cronos.Benchmark/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using BenchmarkDotNet.Running;
+using Cronos;
+using Cronos.Benchmark;
 
 namespace Cronos.Benchmark
 {
@@ -7,8 +9,20 @@ namespace Cronos.Benchmark
     {
         static void Main(string[] args)
         {
+#if DEBUG
+            CronExpression.Parse("* * * * *").GetOccurrence()
+            var now = DateTime.UtcNow;
+            var end = now.AddYears(100);
+            var endTest = now.AddSeconds(20);
+            var exp = CronExpression.Parse("* * * * *");
+            while (endTest > DateTime.UtcNow)
+            {
+                exp.GetOccurrence(DateTime.UtcNow.Date, end, TimeZoneInfo.Utc);
+            }
+#else
             BenchmarkRunner.Run<CronBenchmarks>();
             Console.ReadLine();
+#endif
         }
     }
 }
