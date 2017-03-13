511cb729fef244f72c6eae5a3e2b77e3bd83312f
Sergey Odinokov <odinserj@gmail.com>
2017-03-13 16:51:30 +0300

    Remove the DeBruijn class, inline its method to FindFirstSet

--- src/Cronos/CronExpression.cs
+++ src/Cronos/CronExpression.cs
@@ -14,6 +14,18 @@ namespace Cronos
         private const int MaxNthDayOfWeek = 5;
         private const int SundayBits = 0b1000_0001;
 
+        private static readonly int[] DeBruijnPositions =
+        {
+            0, 1, 2, 53, 3, 7, 54, 27,
+            4, 38, 41, 8, 34, 55, 48, 28,
+            62, 5, 39, 46, 44, 42, 22, 9,
+            24, 35, 59, 56, 49, 18, 29, 11,
+            63, 52, 6, 26, 37, 40, 33, 47,
+            61, 45, 43, 21, 23, 58, 17, 10,
+            51, 25, 36, 32, 60, 20, 57, 16,
+            50, 31, 19, 15, 30, 14, 13, 12
+        };
+
         private long _second;     // 60 bits -> from 0 bit to 59 bit in Int64
         private long _minute;     // 60 bits -> from 0 bit to 59 bit in Int64
         private long _hour;       // 24 bits -> from 0 bit to 23 bit in Int64
@@ -467,7 +479,17 @@ namespace Cronos
         {
             if (startBit <= endBit && GetBit(value, startBit)) return startBit;
 
-            return DeBruijin.FindFirstSet(value, startBit, endBit);
+            // TODO: Add description and source
+
+            value = value >> startBit;
+            if (value == 0) return -1;
+
+            ulong res = unchecked((ulong)(value & -value) * 0x022fdd63cc95386d) >> 58;
+
+            var result = DeBruijnPositions[res] + startBit;
+            if (result > endBit) return -1;
+
+            return result;
         }
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
