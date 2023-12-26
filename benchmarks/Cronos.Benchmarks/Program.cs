1e49578d4fc59d78e12ab4a000f8b4399bb2f9a6
Sergey Odinokov <odinserj@hangfire.io>
2023-12-26 13:50:41 +0700

    Update Program.cs

--- benchmarks/Cronos.Benchmarks/Program.cs
+++ benchmarks/Cronos.Benchmarks/Program.cs
@@ -40,9 +40,9 @@ namespace Cronos.Benchmarks
         }
     }
 
-    class Program
+    public static class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
             /*var benchmarks = new CronBenchmarks();
 
