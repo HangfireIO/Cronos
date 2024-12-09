2f201be9b479585715023e9a4a7c8f6e833b8707
Sergey Odinokov <odinserj@hangfire.io>
2024-12-09 12:49:40 +0700

    Bump version to 0.9.0

--- src/SharedAssemblyInfo.cs
+++ src/SharedAssemblyInfo.cs
@@ -11,4 +11,4 @@ using System.Runtime.InteropServices;
 [assembly: CLSCompliant(true)]
 
 // Please don't edit it manually, use the `build.bat version` command instead.
-[assembly: AssemblyVersion("0.8.4")]
+[assembly: AssemblyVersion("0.9.0")]
