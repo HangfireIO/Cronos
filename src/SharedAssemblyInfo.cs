24305be442a04872149ba7b926dc0683871a7ca9
Sergey Odinokov <odinserj@hangfire.io>
2025-04-08 15:43:25 +0700

    Bump version to 0.10.0 and update copyright year

--- src/SharedAssemblyInfo.cs
+++ src/SharedAssemblyInfo.cs
@@ -4,11 +4,11 @@ using System.Runtime.InteropServices;
 
 [assembly: AssemblyProduct("Cronos")]
 [assembly: AssemblyCompany("Hangfire OÜ")]
-[assembly: AssemblyCopyright("Copyright © 2016-2024 Hangfire OÜ")]
+[assembly: AssemblyCopyright("Copyright © 2016-2025 Hangfire OÜ")]
 [assembly: AssemblyCulture("")]
 
 [assembly: ComVisible(false)]
 [assembly: CLSCompliant(true)]
 
 // Please don't edit it manually, use the `build.bat version` command instead.
-[assembly: AssemblyVersion("0.9.0")]
+[assembly: AssemblyVersion("0.10.0")]
